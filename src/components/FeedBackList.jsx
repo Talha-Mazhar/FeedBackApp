532f2fe5e66b91eedb721854a8f61222c8cc5500
Talha Mazhar <102406980+Talha-Mazhar@users.noreply.github.com>
2023-01-12 22:17:56 +0500

    Update FeedBackList.jsx

--- src/components/FeedBackList.jsx
+++ src/components/FeedBackList.jsx
@@ -20,6 +20,7 @@ function FeedBackList() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            layout
           >
             <FeedbackItem key={item.id} item={item} />
           </motion.div>
